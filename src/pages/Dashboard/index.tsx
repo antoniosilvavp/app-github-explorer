1fd373638cbca8864367750e3879b2d0496b4220
antoniosilvavp <antoniopsilva083@gmail.com>
2021-02-18 01:01:27 -0300

    dashboard styling created

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import githubLogo from '../../assets/github-logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={githubLogo} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="Imagem">
+          <img
+            src="https://avatars.githubusercontent.com/u/64321026?s=460&u=ef2715daef4e36421dd0d07b72aa82241edeff8f&v=4"
+            alt="Antonio Silva"
+          />
+          <div>
+            <strong>antoniosilvavp/conceitos-reactjs</strong>
+            <p>
+              Desafio 03: Conceitos ReactJS aplicado no Bootcamp GoStack13 da
+              Rocketseat!
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
